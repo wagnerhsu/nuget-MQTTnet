b3a160e705b5ca064f49e872bf37bf851caad59b
Wagner Xu <wagnerhsu@hotmail.com>
2020-04-03 07:59:22 +0800

    Add MyCode

---
+++ MyCode/ConsoleAppNet461/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+
+namespace ConsoleAppNet461
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestPublish();
+            Console.WriteLine("Press ENTER to exit");
+            Console.Read();
+        }
+
+        private static void TestPublish()
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true).Build();
+            var mqttSetting = configuration.GetSection("MqttSetting").Get<Dgw.Netstandard.Common.Models.MqttConfigurationModel>();
+            Console.WriteLine(JsonConvert.SerializeObject(mqttSetting));
+        }
+    }
+}
